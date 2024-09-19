1402ad596b37088276979aae6f70e11a1633b884
ABid hasan <abeydhasan134@gmail.com>
2024-09-19 15:09:10 +0600

    populer card with dumi data

--- src/components/homePage/welcome.jsx
+++ src/components/homePage/welcome.jsx
@@ -1,8 +1,11 @@
+import PopularQuize from "./popular/popularQuize";
 
 const Welcome = () => {
     return (
         <div>
             Welcome to QuizPluse
+            <h1 className='text-3xl font-bold m-2 p-2'>Popular Quize</h1>
+            <PopularQuize></PopularQuize>
         </div>
     );
 };
