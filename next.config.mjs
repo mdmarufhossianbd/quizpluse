6eb30b44a1b537656cd18776f6e200879d198690
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-09-20 19:01:22 +0600

    marge with maruf

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['encrypted-tbn0.gstatic.com', 'news.mit.edu'],
+        domains: ['encrypted-tbn0.gstatic.com', 'news.mit.edu', 'd31xsmoz1lk3y3.cloudfront.net'],
       },
 };
 
