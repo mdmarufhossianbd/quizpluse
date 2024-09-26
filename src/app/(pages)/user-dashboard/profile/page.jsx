9f24dc4c7db4c44cd5a3c6984c4bc58a5b647ac9
Abrarulctg <jubairctg99@gmail.com>
2024-09-26 18:16:24 +0600

    added background image on ser profile

--- src/app/(pages)/user-dashboard/profile/page.jsx
+++ src/app/(pages)/user-dashboard/profile/page.jsx
@@ -9,35 +9,40 @@ const page = () => {
     const { data } = useSession()
 
     return (
-        <div>
-            <div className="flex justify-center items-center min-h-screen p-8">
-                <div className="bg-white rounded-lg shadow-lg p-10 w-3/5  text-center relative">
+        <div className='rounded-xl' style={{
+            backgroundImage: 'url("/assets/quizpulse.png")',
+            backgroundSize: "cover", // Use "contain" to keep the image within its original aspect ratio
+            backgroundPosition: "center", // Center the image
+            backgroundRepeat: "no-repeat", // Prevent image from repeating
+        }}>
+            <div className="flex justify-center items-center min-h-screen p-8" >
+                <div className="bg-white rounded-lg shadow-lg  p-10 w-full md:w-3/5  text-center relative mt-10">
 
                     {/* Profile Image */}
-                    <div className="relative mx-auto w-24 h-24 mb-4 rounded-full overflow-hidden border-4 border-white shadow-lg">
+                    <div className="absolute mx-auto -top-20 left-[42%] w-32 h-32 mb-4 rounded-full overflow-hidden border-4 border-white shadow-lg">
                         <Image src={data?.user?.image} alt="User Image" width={96} height={96} className="w-full h-full object-cover" />
                     </div>
 
                     {/* User Info */}
                     <div className='my-8'>
-                        <h2 className="text-2xl font-semibold text-[#5C0096]">{data?.user?.name}</h2>
+                        <h2 className="text-2xl font-semibold text-gray-800">{data?.user?.name}</h2>
                         <p className="text-gray-500 mb-4">{data?.user?.email}</p>
                     </div>
 
 
                     {/* Stats Section */}
-                    <div className="flex justify-between items-center text-gray-600 mb-8 shadow p-4 rounded-xl">
-                        <div className="text-center">
-                            <span className="block text-lg font-semibold">65</span>
-                            <span className="text-xs">Quizzes</span>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center text-gray-600 mb-8 shadow p-4 rounded-xl">
+                        <div className="text-center mb-2">
+                            <span className="block text-xl font-semibold text-[#5C0096]">65</span>
+                            <span className="text-sm">Quizzes</span>
                         </div>
-                        <div className="text-center">
-                            <span className="block text-lg font-semibold">43</span>
-                            <span className="text-xs">Participated Quizzes</span>
+                        <div className="text-center mb-2">
+                            <span className="block text-xl font-semibold text-[#5C0096]">43</span>
+                            <span className="text-sm">Participated Quizzes</span>
                         </div>
-                        <div className="text-center">
-                            <span className="block text-lg font-semibold">21</span>
-                            <span className="text-xs">Wins</span>
+                        <div className="text-center mb-2">
+                            <span className="block text-xl font-semibold text-[#5C0096]">21</span>
+                            <span className="text-sm">Wins</span>
                         </div>
                     </div>
 
@@ -65,3 +70,4 @@ const page = () => {
 };
 
 export default page;
+
