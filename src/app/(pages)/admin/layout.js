6002ec68822547dad5205ae8a0467d4c753def6f
hagghat <mahach0396@gmail.com>
2024-09-27 18:23:51 +0600

    update-contact-page

--- src/app/(pages)/admin/layout.js
+++ src/app/(pages)/admin/layout.js
@@ -1,13 +1,18 @@
+'use client'
 import AdminSidebar from "@/components/admin/AdminSidebar";
+import { usePathname } from "next/navigation";
 
 
 
 export default function AdminLayout({ children }) {
+
+  const path = usePathname();
+
   return (
      <div className="flex flex-col md:flex-row lg:p-4 lg:my-4">
      
        <AdminSidebar/>
-      {children}
+        {children}
      </div>
   );
 }
