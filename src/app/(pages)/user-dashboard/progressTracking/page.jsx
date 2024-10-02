da16458b495d92eb339475813cfb507163a816d3
ABid hasan <abeydhasan134@gmail.com>
2024-10-03 02:23:56 +0600

    progess traking

---
+++ src/app/(pages)/user-dashboard/progressTracking/page.jsx
@@ -0,0 +1,16 @@
+
+import ProgressHistory from '@/components/userDashboard/progressTracking/progressHistory'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ProgressHistory></ProgressHistory>
+    </div>
+  )
+}
+
+export default page
+
+
+  
