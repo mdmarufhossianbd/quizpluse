cab15504e1eeb709d90292a22b94cd1861e165e1
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-10-16 04:23:11 +0600

    subscription ui update

--- src/components/userDashboard/user-plans/planCard.jsx
+++ src/components/userDashboard/user-plans/planCard.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
+import { IconArrowForwardUp } from '@tabler/icons-react'
 import './style.css'
-import ListPlan from './listPlan'
 
-const PlanCard = ({plan}) => {
-    // console.log(plan)
+const PlanCard = ({ plan }) => {
+  // console.log(plan)
+
   return (
-   
-<div class="card m-2 md:min-h-[700px] h-auto">
-    {/* border propoerty */}
-  <div class="card__border"></div>
-  {/* tittle */}
-  <div class="card_title__container">
-    <div className="flex justify-between">
-    <h1 class="card_title lg:text-3xl text-xl">{plan.name} </h1>
-    <p className="text-white lg:text-xl text-xs">{plan.price} <span className='text-xs'>{plan.currency}</span></p>
+    <div className=''>
+      <div className={`h-14 flex justify-center rounded-t-md ${plan.name === 'starter' ? 'bg-purple-200 font-semibold' : ' bg-white'}`}>
+        <p className='h-full flex items-center text-2xl'>{plan.name === 'starter' && 'Recommended'}</p>
+      </div>
+      <div className={`px-5 py-10 bg-gradient-to-t from-[#b966e763] to-[#2f57ef4a] hover:from-[#ba66e7c5] hover:to-[#2f55efc4] duration-400${plan.name === 'starter' && 'border-x-2 border-purple-200 bg-gradient-to-t from-[#2f55efc4] to-[#ba66e7c5] text-white'}`}>
+        <p>Basic</p>
+        <h2 className='text-5xl my-1 font-semibold'>Free</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi expedita quia impedit ex sequi cupiditate minima, facilis consectetur magnam modi?</p>
+        <hr className='my-4' />
+        {
+          plan.features.map((f, idx) => (
+            <li className='list-none flex items-center gap-3 py-1 group' key={idx}>
+              <IconArrowForwardUp stroke={2} className='group-hover:ml-2 duration-300' />
+              <p className='hover:font-semibold'>
+                {f}
+              </p>
+            </li>
+          ))
+        }
+        <div className='flex justify-center mt-5'>
+          <button className='w-[85%] bg-[#6e00ff] rounded text-white font-semibold hover:scale-105 duration-250 py-2'>
+            Upgrade Now
+          </button>
+        </div>
+      </div>
     </div>
-    <p class="card_paragraph text-xl mb-2">
-      {plan.description}
-    </p>
-  </div>
-  <hr class="line" />
-  {/* list */}
-  <ul class="card__list my-5 gap-10  h-[450px] md:h-[500px] overflow-auto">
-    <ListPlan features={plan.features}></ListPlan>
-    
-  </ul>
-  <div className='mx-5 bg-red-600 flex justify-center'>
-  <button class="button text-3xl bg-[#6d28ff] text-white rounded-md w-[90%] hover:bg-white hover:text-[#6d28ff]">Subscribe Now</button>
-  </div>
-</div>
 
   )
 }
