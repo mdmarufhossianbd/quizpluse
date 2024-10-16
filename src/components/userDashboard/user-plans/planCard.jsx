58ed610f89b53302b6335f598d6e7deb03ce2b93
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-10-17 05:28:33 +0600

    payment added successfully

--- src/components/userDashboard/user-plans/planCard.jsx
+++ src/components/userDashboard/user-plans/planCard.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useState } from 'react';
 import { IconArrowForwardUp } from '@tabler/icons-react';
-import './style.css';
+import { useState } from 'react';
 import PaymentModalWrapper from './paymentModal';
+import './style.css';
 
 const PlanCard = ({ plan }) => {
   const [isModalOpen, setIsModalOpen] = useState(false); // State to toggle modal
@@ -18,40 +18,47 @@ const PlanCard = ({ plan }) => {
         {/* Plan Header */}
         <div
           className={`h-14 flex justify-center items-center rounded-t-md text-white 
-          ${plan.name === 'starter' && 'bg-[#3a005e] font-bold'}`}
+          ${plan.name === 'starter' && 'bg-gradient-to-r from-purple-600 to-indigo-600 font-bold'}`}
         >
           {plan.name === 'starter' && <p className="uppercase">Recommended</p>}
         </div>
 
         {/* Plan Body */}
         <div
-          className={`px-6 py-8 bg-gradient-to-t from-gray-100 to-white 
-          hover:shadow-xl hover:from-gray-200 transition-all duration-300 hover:cursor-pointer 
-          ${plan.name === 'starter' ? 'border-2 border-[#5C0096] bg-gradient-to-t from-[#2f55efc4] to-[#ba66e7c5] text-white' : ''}`}
+          className={`px-6 py-8 hover:bg-gradient-to-t from-[#f2e9f1] to-white ${plan.name === 'starter' ? 'border-2 border-[#5c009693] bg-gradient-to-t from-[#f2e9f1] to-white text-white' : ''}`}
         >
-          <p className="text-sm text-gray-500 uppercase">Basic Plan</p>
-          <h2 className="text-5xl my-3 font-semibold text-gray-900">
-            Free
-          </h2>
+          <p className="text-sm text-gray-500 uppercase">{plan.title} Plan</p>
+          {
+            plan.name === 'basic' ? <h2 className="text-5xl my-3 font-semibold text-gray-900">
+              Basic
+            </h2> : plan.name === 'starter' ? <div>
+              <h2 className="text-5xl my-3 font-semibold text-gray-900">
+                {plan.price} <span className='text-lg'>/monthly</span>
+              </h2>
+            </div> : <div>
+              <h2 className="text-5xl my-3 font-semibold text-gray-900">
+                {plan.price} <span className='text-lg'>/one time</span>
+              </h2>
+            </div>
+          }
           <p className="text-gray-700 leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi expedita
-            quia impedit ex sequi cupiditate minima, facilis consectetur magnam modi?
+            {plan.description}
           </p>
 
-          <hr className="my-6 border-gray-300" />
+          <hr className="my-4 border-gray-300" />
 
           {/* Features */}
           <ul className="space-y-2">
             {plan.features.map((feature, idx) => (
               <li key={idx} className="flex items-center gap-3 py-1 group text-gray-800">
-                <IconArrowForwardUp stroke={1} className="group-hover:ml-1 transition-transform duration-300" />
+                <IconArrowForwardUp stroke={1} className="group-hover:ml-1 duration-300" />
                 <span>{feature}</span>
               </li>
             ))}
           </ul>
 
           {/* Upgrade Button */}
-          <div className="flex justify-center mt-6">
+          <div className="mt-5 flex justify-center">
             <button
               onClick={handleUpgrade}
               className="w-full bg-gradient-to-r from-purple-600 to-indigo-600 text-white py-3 rounded-md font-semibold hover:scale-105 transition-transform duration-300"
