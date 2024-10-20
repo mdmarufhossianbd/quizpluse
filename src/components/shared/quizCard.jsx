d942313db4122b42144a17136cff184fade7e9db
Abrarulctg <jubairctg99@gmail.com>
2024-10-20 22:46:17 +0600

    Added Dynamic Featured Shape on Featured quiz

--- src/components/shared/quizCard.jsx
+++ src/components/shared/quizCard.jsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 
 const QuizCard = ({ item }) => {
     return (
-        <div className="text-start group p-5 bg-[#D6A0F8]/10 rounded-lg flex flex-col  justify-center gap-2 relative after:absolute after:h-full after:bg-[#dcabf84b] z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0">
+        <div className="text-start group p-5 bg-[#D6A0F8]/10 rounded-lg flex flex-col justify-center gap-2 relative after:absolute after:h-full after:bg-[#dcabf84b] z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0">
             <Image
                 className="rounded group-hover:opacity-50 group-hover:scale-105 duration-200 w-full"
                 src={item?.quizImage}
@@ -13,9 +13,10 @@ const QuizCard = ({ item }) => {
                 height={200}
             />
             <p className="font-semibold tracking-wider group-hover:text-gray-700 text-xl">
-                {item?.quizName}</p>
+                {item?.quizName}
+            </p>
             <div className="flex justify-between">
-                <p>Questions : {item?.totalQuestions} </p>
+                <p>Questions: {item?.totalQuestions}</p>
                 <p>Duration: {item?.quizDuration} minutes</p>
             </div>
             <div className="flex justify-between text-xs">
@@ -26,7 +27,12 @@ const QuizCard = ({ item }) => {
             <Button className="hidden group-hover:flex absolute left-[40%] top-[40%]">
                 <Link href={`/quizes/${item?._id}`}>Start Now</Link>
             </Button>
-            {item.featured === "Yes" && <p >Featured</p>}
+
+            {item.featured === "Yes" && (
+                <p className="absolute top-5 -left-7 bg-purple-600 text-white text-sm px-8 py-1 transform -rotate-45 shadow-lg">
+                    Featured
+                </p>
+            )}
         </div>
     );
 };
