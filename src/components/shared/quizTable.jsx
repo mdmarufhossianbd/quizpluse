3b2bac6eac7ec45a29d5966e6c6c8bc4e9ba1fb6
ABid hasan <abeydhasan134@gmail.com>
2024-10-21 23:30:52 +0600

    status to Admin

--- src/components/shared/quizTable.jsx
+++ src/components/shared/quizTable.jsx
@@ -1,4 +1,3 @@
-'use client'
 import { Chip, Tooltip } from "@nextui-org/react";
 import {
   Table,
@@ -13,23 +12,10 @@ import Link from "next/link";
 import PreviewModal from "../admin/manageQuiz/previewModal";
 import DeleteQuiz from "../userDashboard/manageQuiz/deleteQuiz";
 import MakeFeatured from "./makeFeatured";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "radix-ui";
-import { FaChevronDown } from "react-icons/fa";
-import axios from 'axios';
+
 
 const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
-  const handelStatus=async(status,id)=>{
-    console.log(status)
-    try{
-      const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
-      const _id=id
-      const res = await axios.put(`${baseURL}/api/v1/quiz/change-satus`,{_id:_id,quizStatus:status})
-      console.log(res.data);
-    }
-    catch(error){
-      console.log(error);
-    }
-  }
+ 
   return (
     <Table>
       <TableHeader>
@@ -65,44 +51,10 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
                 </Chip>
               )}
                {item.quizStatus === "pending" && (
-                < div className="flex">
                   <Chip color="primary" variant="dot" className="border-none">
                     Pending
                   </Chip>
-                  <div style={{ position: "relative" }}>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger>
-                        <button className="btn text-sm p-2 ">
-                          <FaChevronDown></FaChevronDown>
-                        </button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent
-                        style={{
-                          position: "absolute",
-                          top: "100%",
-                          left: 0,
-                          zIndex: 10,
-                          backgroundColor: "white",
-                          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-                          padding: "0.5rem",
-                          borderRadius: "0.375rem",
-                        }}
-                      >
-                        <DropdownMenuLabel>
-                          <button onClick={()=>handelStatus('publish',item._id)} className="btn p-2 hover:bg-[#5C1296] hover:text-white">
-                            Publish
-                          </button>
-                        </DropdownMenuLabel>
-                        <DropdownMenuLabel>
-                          <button onClick={()=>handelStatus('rejected',item._id)} className="btn p-2 hover:bg-[#5C1296] hover:text-white">
-                            Reject
-                          </button>
-                        </DropdownMenuLabel>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                </ div>
-              )}
+                )}
               {item.quizStatus === "rejected" && (
                 <Chip color="danger" variant="dot" className="border-none">
                   Rejected
