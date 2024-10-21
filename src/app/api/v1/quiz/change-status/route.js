981e3f51a0abfdb0ec613b82671c1b73d07725f2
ABid hasan <abeydhasan134@gmail.com>
2024-10-21 22:33:29 +0600

    states management

--- src/app/api/v1/quiz/change-status/route.js
+++ src/app/api/v1/quiz/change-status/route.js
@@ -3,26 +3,25 @@ import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function PUT(request) {
-    const db = await connectDB();
-    const quizCollection = db.collection('quizes');
-    try {
-        const data = await request.json()
-        const query = {_id : new ObjectId(data._id)}
-        const oparation = {
-            $set : {
-                quizStatus : data.quizStatus
-            }
-        }
-        const result = await quizCollection.updateOne(query, oparation);
-        return NextResponse.json({
-            message : 'Quiz status update successfully',
-            status : 200,
-            result,
-            success : true
-        })
-    } catch (error) {
-        return NextResponse.json({
-            message : 'Something went wrong'
-        })
-    }
+  const db = await connectDB();
+  const quizCollection = db.collection('quizes');
+  try {
+    const data = await request.json();
+    const query = { _id: new ObjectId(data._id) };
+    const operation = {
+      $set: { quizStatus: data.quizStatus },
+    };
+    const result = await quizCollection.updateOne(query, operation);
+    return NextResponse.json({
+      message: 'Quiz status updated successfully',
+      status: 200,
+      result,
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: 'Something went wrong',
+      status: 500,
+    });
+  }
 }
--- src/components/shared/quizTable.jsx
+++ src/components/shared/quizTable.jsx
@@ -1,13 +1,35 @@
+'use client'
 import { Chip, Tooltip } from "@nextui-org/react";
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/table";
 import { IconEdit } from "@tabler/icons-react";
 import Link from "next/link";
 import PreviewModal from "../admin/manageQuiz/previewModal";
 import DeleteQuiz from "../userDashboard/manageQuiz/deleteQuiz";
 import MakeFeatured from "./makeFeatured";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "radix-ui";
+import { FaChevronDown } from "react-icons/fa";
+import axios from 'axios';
 
 const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
-  
+  const handelStatus=async(status,id)=>{
+    console.log(status)
+    try{
+      const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+      const _id=id
+      const res = await axios.put(`${baseURL}/api/v1/quiz/change-satus`,{_id:_id,quizStatus:status})
+      console.log(res.data);
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
   return (
     <Table>
       <TableHeader>
@@ -21,7 +43,7 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
         <TableColumn>Actions</TableColumn>
       </TableHeader>
       <TableBody>
-        {quizzes.map((item, idx) => (
+        {quizzes?.map((item, idx) => (
           <TableRow key={idx}>
             <TableCell>{idx + 1}</TableCell>
             <TableCell>{item.quizName}</TableCell>
@@ -29,7 +51,12 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
             <TableCell>{item.totalQuestions}</TableCell>
             <TableCell>{item.quizDuration}</TableCell>
             <TableCell>
-              <MakeFeatured quizId={item._id} isFeatured={item.featured} email={email} setFeatured={setFeatured} />
+              <MakeFeatured
+                quizId={item._id}
+                isFeatured={item.featured}
+                email={email}
+                setFeatured={setFeatured}
+              />
             </TableCell>
             <TableCell>
               {item.quizStatus === "publish" && (
@@ -37,10 +64,44 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
                   Published
                 </Chip>
               )}
-              {item.quizStatus === "pending" && (
-                <Chip color="primary" variant="dot" className="border-none">
-                  Pending
-                </Chip>
+               {item.quizStatus === "pending" && (
+                < div className="flex">
+                  <Chip color="primary" variant="dot" className="border-none">
+                    Pending
+                  </Chip>
+                  <div style={{ position: "relative" }}>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <button className="btn text-sm p-2 ">
+                          <FaChevronDown></FaChevronDown>
+                        </button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        style={{
+                          position: "absolute",
+                          top: "100%",
+                          left: 0,
+                          zIndex: 10,
+                          backgroundColor: "white",
+                          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                          padding: "0.5rem",
+                          borderRadius: "0.375rem",
+                        }}
+                      >
+                        <DropdownMenuLabel>
+                          <button onClick={()=>handelStatus('publish',item._id)} className="btn p-2 hover:bg-[#5C1296] hover:text-white">
+                            Publish
+                          </button>
+                        </DropdownMenuLabel>
+                        <DropdownMenuLabel>
+                          <button onClick={()=>handelStatus('rejected',item._id)} className="btn p-2 hover:bg-[#5C1296] hover:text-white">
+                            Reject
+                          </button>
+                        </DropdownMenuLabel>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                </ div>
               )}
               {item.quizStatus === "rejected" && (
                 <Chip color="danger" variant="dot" className="border-none">
@@ -57,7 +118,9 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
               <Tooltip content="Edit">
                 <span className="text-lg cursor-pointer active:opacity-50">
                   <button>
-                    <Link href={`/user-dashboard/manage-quiz/edit-quiz/${item?._id}`}>
+                    <Link
+                      href={`/user-dashboard/manage-quiz/edit-quiz/${item?._id}`}
+                    >
                       <IconEdit stroke={2} />
                     </Link>
                   </button>
@@ -65,7 +128,11 @@ const QuizTable = ({ quizzes, setDelete, email, setFeatured }) => {
               </Tooltip>
               <Tooltip color="danger" content="Delete Quiz">
                 <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <DeleteQuiz id={item?._id} setDelete={setDelete} quizName={item?.quizName} />
+                  <DeleteQuiz
+                    id={item?._id}
+                    setDelete={setDelete}
+                    quizName={item?.quizName}
+                  />
                 </span>
               </Tooltip>
             </TableCell>
