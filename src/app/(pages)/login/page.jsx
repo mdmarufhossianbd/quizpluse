02f4981641ca1d8416a2c207365016acb28a3e32
Abrarulctg <jubairctg99@gmail.com>
2024-10-24 18:22:14 +0600

    Fixed Margin issue on Login Page

--- src/app/(pages)/login/page.jsx
+++ src/app/(pages)/login/page.jsx
@@ -7,15 +7,15 @@ import LoginAnimation from "@/components/shared/loginAnimation";
 
 const Login = () => {
     return (
-        <div className="my-6 md:my-14 lg:my-16 xl:my-20">
+        <div className="my-6 md:my-14 lg:my-10 xl:my-16 mx-4">
             <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-10 items-center bg-[#f5f5f5] rounded-md">
-                <div className="flex justify-center w-full md:w-1/2 ">
+                <div className="flex justify-center w-full md:w-1/2 p-10 md:p-4">
                     {/* Lottie Animation */}
                     <LoginAnimation />
 
                     {/* <Image className="w-full h-[700px] rounded-l-md" src={loginImage} width={500} height={800} alt="login" unoptimized priority /> */}
                 </div>
-                <div className="w-full md:w-1/2 md:p-4 lg:p-8">
+                <div className="w-full md:w-1/2 p-2 md:p-4 lg:p-8">
                     <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">Welcome to QuizPulse</h2>
                     <h2 className="font-semibold text-2xl md:text-2xl lg:text-3xl text-center my-6">Please Login</h2>
                     <LoginForm />
