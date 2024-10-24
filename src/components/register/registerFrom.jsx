32ccd15fcc25d4ac48f50074fab733db71e3a772
Abrarulctg <jubairctg99@gmail.com>
2024-10-24 19:19:13 +0600

    Declared state for visible and confirm visible password

--- src/components/register/registerFrom.jsx
+++ src/components/register/registerFrom.jsx
@@ -11,40 +11,40 @@ const RegisterFrom = () => {
     const [loading, setLoading] = useState(false)
     const router = useRouter();
 
-    const handleRegister = async(e) => {
+    const handleRegister = async (e) => {
         setLoading(true)
         e.preventDefault()
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const confirmPassword = e.target.confirmPassword.value
-        if(!email || !password){
+        if (!email || !password) {
             setLoading(false)
             return toast.error('Please enter email and password.')
         }
-        if(password.length < 6){
+        if (password.length < 6) {
             setLoading(false)
             return toast.error("Password at least 6 characters")
         }
-        if(password !== confirmPassword ){
+        if (password !== confirmPassword) {
             setLoading(false)
             return toast.error("Don't match your password.")
         }
-        const loginInfo = {name, email, password}
-        
+        const loginInfo = { name, email, password }
+
         await axios.post('/api/v1/register', loginInfo)
-        .then(res => {
-            if(res.data.success){
+            .then(res => {
+                if (res.data.success) {
+                    setLoading(false)
+                    toast.success(res.data.message)
+                    e.target.reset()
+                    router.push('/login')
+                } else if (res.data.status === 406) {
+                    setLoading(false)
+                    toast.error(res.data.message)
+                }
                 setLoading(false)
-                toast.success(res.data.message)
-                e.target.reset()
-                router.push('/login')
-            } else if(res.data.status === 406) {
-                setLoading(false)
-                toast.error(res.data.message)
-            }
-            setLoading(false)
-        })
+            })
     }
     return (
         <div className="lg:w-2/3 w-full mx-auto">
@@ -53,11 +53,21 @@ const RegisterFrom = () => {
                 <label className='font-semibold'>Your Full Name</label>
                 <input className="border focus:outline-none px-5 py-2 rounded" type="name" name="name" placeholder="Enter Full Name " required />
                 <label className='font-semibold'>Your Email</label>
-                <input className="border focus:outline-none px-5 py-2 rounded" type="email" name="email" placeholder="Enter email"  />
+                <input className="border focus:outline-none px-5 py-2 rounded" type="email" name="email" placeholder="Enter email" />
                 <label className='font-semibold'>Your Password</label>
-                <input className="border focus:outline-none px-5 py-2 rounded" type="password" name="password" placeholder="Enter password" />
+                <input
+                    className="border focus:outline-none px-5 py-2 rounded"
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                />
                 <label className='font-semibold'>Confirm Password</label>
-                <input className="border focus:outline-none px-5 py-2 rounded" type="password" name="confirmPassword" placeholder="Enter password" />
+                <input
+                    className="border focus:outline-none px-5 py-2 rounded"
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Enter password"
+                />
                 {
                     loading ? <button disabled className='cursor-not-allowed flex items-center gap-2 justify-center py-2 rounded border     bg-[#5C0096] hover:bg-[#500081] text-white border-none'><ImSpinner9 className='animate-spin' /> Creating account</button> : <input className="py-2 rounded border hover:cursor-pointer bg-[#5C0096] hover:bg-[#500081] text-white border-none" type="submit" value="Register" />
                 }
