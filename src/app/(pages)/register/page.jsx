e8a95c5079d2fe7701b37902dccff7555e7f8592
Abrarulctg <jubairctg99@gmail.com>
2024-10-24 19:05:18 +0600

    Added LottieAnimation on Register Page

--- src/app/(pages)/register/page.jsx
+++ src/app/(pages)/register/page.jsx
@@ -1,10 +1,11 @@
 import RegisterFrom from "@/components/register/registerFrom";
 import Image from "next/image";
 import loginImage from '../../../../public/assets/login.webp';
+import LoginAnimation from "@/components/shared/loginAnimation";
 
 const Register = () => {
     return (
-        <div className="my-10 md:my-14 lg:my-16 xl:my-20 px-5">
+        <div className="my-6 md:my-14 lg:my-10 xl:my-16 mx-4">
             <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row gap-10 items-center bg-[#f5f5f5] rounded-md px-5 md:px-0 md:pl-5">
                 <div className="md:w-1/2 w-full">
                     <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">Welcome to QuizePluse</h2>
@@ -12,7 +13,8 @@ const Register = () => {
                     <RegisterFrom />
                 </div>
                 <div className="md:w-1/2 w-full">
-                    <Image className="w-full h-[800px] md:rounded-r-md rounded-t-md" src={loginImage} width={500} height={700} alt="login" unoptimized priority />
+                    <LoginAnimation />
+                    {/* <Image className="w-full h-[800px] md:rounded-r-md rounded-t-md" src={loginImage} width={500} height={700} alt="login" unoptimized priority /> */}
                 </div>
             </div>
         </div>
