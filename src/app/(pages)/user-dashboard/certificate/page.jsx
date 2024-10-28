fbc0e76948a0850023cec8bd1a9d68eec01a7f02
Abrarulctg <jubairctg99@gmail.com>
2024-10-28 20:11:09 +0600

    Added Certificate route on for Certificate design purpose

---
+++ src/app/(pages)/user-dashboard/certificate/page.jsx
@@ -0,0 +1,12 @@
+import CertificatePage from '@/components/userDashboard/certificate/certificatePage';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <CertificatePage />
+        </div>
+    );
+};
+
+export default page;
---
+++ src/components/userDashboard/certificate/certificatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CertificatePage = () => {
+    return (
+        <div>
+            This is Certificate Page
+        </div>
+    );
+};
+
+export default CertificatePage;
