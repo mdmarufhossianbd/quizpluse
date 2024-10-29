255b287df99239823d853d3847992be43a8b485e
Abrarulctg <jubairctg99@gmail.com>
2024-10-29 11:40:06 +0600

    Fixed landscape download issue

--- src/components/userDashboard/certificate/certificatePage.jsx
+++ src/components/userDashboard/certificate/certificatePage.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Certificate from './certificate';
 
 const CertificatePage = () => {
+
     return (
         <div>
             This is Certificate Page
             <Certificate
-                userName="John Doe"
                 courseName="QuizPulse 10 Quizzes and Achieved 80% Marks"
                 issueDate="October 27, 2024"
                 certificateId="671e7f073ae2d"
